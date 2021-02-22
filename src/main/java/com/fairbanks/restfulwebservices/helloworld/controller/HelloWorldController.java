c17e17cc25c69674472d263101de13b52dd9087a
Gabriel Fairbanks <gabriel.fairbanks@gmail.com>
2021-02-22 08:58:11 +0000

    Section 3 - Step 18

--- src/main/java/com/fairbanks/restfulwebservices/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/fairbanks/restfulwebservices/helloworld/controller/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.fairbanks.restfulwebservices.helloworld.controller;
 
 import com.fairbanks.restfulwebservices.helloworld.model.HelloWorldBean;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
@@ -21,6 +31,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s",name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorld(@RequestHeader(name = "Accept-Language", required = false)Locale locale){
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
 
 }
