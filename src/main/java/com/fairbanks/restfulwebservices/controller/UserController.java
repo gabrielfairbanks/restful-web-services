f77c36d7101caa1cce59894e70283e2ca2c9f3ec
Gabriel Fairbanks <gabriel.fairbanks@gmail.com>
2021-02-22 08:24:44 +0000

    Section 3 - Step 15

--- src/main/java/com/fairbanks/restfulwebservices/controller/UserController.java
+++ src/main/java/com/fairbanks/restfulwebservices/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -49,7 +50,7 @@ public class UserController {
     }
 
     @PostMapping(path = "/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user){
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
         Integer id = user.getId();
         if(id != null && userDaoService.findOne(id) != null){
             throw new UserAlreadyExistsException("User Id: "+ id + " already exists");
--- src/main/java/com/fairbanks/restfulwebservices/model/User.java
+++ src/main/java/com/fairbanks/restfulwebservices/model/User.java
@@ -1,27 +1,30 @@
 package com.fairbanks.restfulwebservices.model;
 
 import lombok.Data;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-@RequiredArgsConstructor
 public class User {
 
-    @NonNull
     private Integer id;
 
-    @NonNull
+    @Size(min = 2, message = "Name should have at least 2 characters")
     private String name;
 
-    @NonNull
+    @Past(message = "Birth date needs to be in the past, is the user not born yet, punk?")
     private Date birthDate;
 
 
-    private final List<Post> posts = new ArrayList<>();
+    private List<Post> posts = new ArrayList<>();
 
+    public User(Integer id, String name, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
 }
