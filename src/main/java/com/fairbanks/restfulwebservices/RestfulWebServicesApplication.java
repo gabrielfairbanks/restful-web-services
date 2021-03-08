2ae457903ff5edff726fb389a3067862336d3d19
Gabriel Fairbanks <gabriel.fairbanks@gmail.com>
2021-03-08 07:21:01 +0000

    Lesson 35

--- src/main/java/com/fairbanks/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/fairbanks/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,9 +3,8 @@ package com.fairbanks.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 import java.util.Locale;
 
@@ -19,17 +18,10 @@ public class RestfulWebServicesApplication {
 
 	@Bean
 	public LocaleResolver localeResolver(){
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.UK);
 
 		return localeResolver;
-	}
-
-	@Bean
-	public ResourceBundleMessageSource bundleMessageSource(){
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasename("messages");
-		return messageSource;
 	}
 
 }
--- src/main/java/com/fairbanks/restfulwebservices/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/fairbanks/restfulwebservices/helloworld/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.fairbanks.restfulwebservices.helloworld.controller;
 
 import com.fairbanks.restfulwebservices.helloworld.model.HelloWorldBean;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -34,8 +35,8 @@ public class HelloWorldController {
     }
 
     @GetMapping(path = "/hello-world-internationalized")
-    public String helloWorld(@RequestHeader(name = "Accept-Language", required = false)Locale locale){
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String helloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 
 }
