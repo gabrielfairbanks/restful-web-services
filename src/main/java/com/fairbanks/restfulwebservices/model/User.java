f177de0ecae10f86bab517cf23553710f5fc2583
Gabriel Fairbanks <gabriel.fairbanks@gmail.com>
2021-03-08 08:06:21 +0000

    Lesson 40

--- src/main/java/com/fairbanks/restfulwebservices/model/User.java
+++ src/main/java/com/fairbanks/restfulwebservices/model/User.java
@@ -1,5 +1,7 @@
 package com.fairbanks.restfulwebservices.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.Past;
@@ -9,14 +11,17 @@ import java.util.Date;
 import java.util.List;
 
 @Data
+@ApiModel(description = "Details about users")
 public class User {
 
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(notes = "Name should have at least 2 characters")
     private String name;
 
     @Past(message = "Birth date needs to be in the past, is the user not born yet, punk?")
+    @ApiModelProperty(notes = "Birth date should be in the past.")
     private Date birthDate;
 
 
