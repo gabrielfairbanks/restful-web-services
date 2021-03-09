9a2b64bb59b52a68f1b7bfa1749485bdfa947f68
Gabriel Fairbanks <gabriel.fairbanks@gmail.com>
2021-03-09 07:33:35 +0000

    Lesson 44

--- src/main/java/com/fairbanks/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/fairbanks/restfulwebservices/filtering/FilteringController.java
@@ -1,6 +1,10 @@
 package com.fairbanks.restfulwebservices.filtering;
 
 import com.fairbanks.restfulwebservices.model.FilteringExampleBean;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +15,32 @@ import java.util.List;
 public class FilteringController {
 
     @GetMapping("/filtering")
-    public FilteringExampleBean retrieveBean(){
-        return new FilteringExampleBean("value1", "value2","value3");
+    public MappingJacksonValue retrieveBean() {
+        FilteringExampleBean filteringExampleBean = new FilteringExampleBean("value1", "value2", "value3");
+
+
+        SimpleBeanPropertyFilter simpleBeanPropertyFilter = SimpleBeanPropertyFilter.filterOutAllExcept("attribute1", "attribute3");
+
+        FilterProvider filterProvider = new SimpleFilterProvider().addFilter("FilteringExampleBeanFilter", simpleBeanPropertyFilter);
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(filteringExampleBean);
+        mappingJacksonValue.setFilters(filterProvider);
+
+        return mappingJacksonValue;
     }
 
     @GetMapping("/filtering-list")
-    public List<FilteringExampleBean> retrieveListOfBeans(){
-        return Arrays.asList(new FilteringExampleBean("value1", "value2","value3"),new FilteringExampleBean("value4", "value5","value6"));
+    public MappingJacksonValue retrieveListOfBeans() {
+        List<FilteringExampleBean> filteringExampleBeans = Arrays.asList(new FilteringExampleBean("value1", "value2", "value3"), new FilteringExampleBean("value4", "value5", "value6"));
+
+
+        SimpleBeanPropertyFilter simpleBeanPropertyFilter = SimpleBeanPropertyFilter.filterOutAllExcept("attribute2", "attribute3");
+
+        FilterProvider filterProvider = new SimpleFilterProvider().addFilter("FilteringExampleBeanFilter", simpleBeanPropertyFilter);
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(filteringExampleBeans);
+        mappingJacksonValue.setFilters(filterProvider);
+
+        return mappingJacksonValue;
     }
 }
--- src/main/java/com/fairbanks/restfulwebservices/model/FilteringExampleBean.java
+++ src/main/java/com/fairbanks/restfulwebservices/model/FilteringExampleBean.java
@@ -1,15 +1,15 @@
 package com.fairbanks.restfulwebservices.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFilter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonFilter("FilteringExampleBeanFilter")
 public class FilteringExampleBean {
     private String attribute1;
     private String attribute2;
-    @JsonIgnore
     private String attribute3;
 
 }
