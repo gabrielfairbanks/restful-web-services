9143ee7392c972d1721dac5981caed83cfea50a2
Gabriel Fairbanks <gabriel.fairbanks@gmail.com>
2021-03-09 07:54:36 +0000

    Lesson 46 - Versioning using produces

--- src/main/java/com/fairbanks/restfulwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/fairbanks/restfulwebservices/versioning/PersonVersioningController.java
@@ -35,4 +35,14 @@ public class PersonVersioningController {
     public PersonV2 personV2header(){
         return new PersonV2(new Name("Claudio", "Fairbanks"));
     }
+
+    @GetMapping(value="person/produces", produces="application/vnd.company.app-v1+json")
+    public PersonV1 personV1produces(){
+        return new PersonV1("Claudio Fairbanks");
+    }
+
+    @GetMapping(value="person/produces", produces="application/vnd.company.app-v2+json")
+    public PersonV2 personV2produces(){
+        return new PersonV2(new Name("Claudio", "Fairbanks"));
+    }
 }
