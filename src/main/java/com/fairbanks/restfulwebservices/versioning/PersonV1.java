9c19c55cfab4576d45d2903455e372572901c04e
Gabriel Fairbanks <gabriel.fairbanks@gmail.com>
2021-03-09 07:45:23 +0000

    Lesson 45

---
+++ src/main/java/com/fairbanks/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,13 @@
+package com.fairbanks.restfulwebservices.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonV1 {
+
+    private String name;
+}
